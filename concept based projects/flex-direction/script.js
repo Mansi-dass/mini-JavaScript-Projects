964425f945ed78c0b466b65f387fdad2d46608d7
Mansi <116175281+Mansi-dass@users.noreply.github.com>
2023-03-06 14:44:47 +0530

    Add files via upload

---
+++ concept based projects/flex-direction/script.js
@@ -0,0 +1,34 @@
+const selectEl = document.querySelector("#select-el")
+const container = document.querySelector(".container")
+const items = document.querySelectorAll(".items")
+// const items = document.querySelector(".items")
+// const items = document.getElementsByClassName("items")
+
+console.log(items)
+let flexDir = ""
+
+selectEl.addEventListener("change", function () {
+    flexDir = selectEl.value
+    if (flexDir === "row" || flexDir === "row-reverse") {
+        container.style.height = "21vh"
+        container.style.alignItems = "center"
+        for (let i = 0; i < items.length; i++) {
+            items[i].style.width = "14vh"
+            items[i].style.height = "14vh"
+            items[i].style.margin = "1.25rem"
+        }
+        container.style.flexDirection = flexDir
+    } else {
+        container.style.height = "75vh"
+        // container.style.alignItems = "none"
+        // container.style.alignItems = 'null'
+        container.style.removeProperty("align-items")
+        
+        for (let i = 0; i < items.length; i++) {
+            items[i].style.width = "11vh"
+            items[i].style.height = "11vh"
+            items[i].style.margin = "0.9rem"
+        }
+        container.style.flexDirection = flexDir
+    }
+})
